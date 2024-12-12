4b08df42622d0ff6eda0b851e5e1823208ce44d7
ANSELM OJIMADU <you@example.com>
2024-12-12 16:29:00 -0500

    Updated App.js file

--- project2-app/src/App.js
+++ project2-app/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Introduction from './components/Introduction';
+import Profiles from './components/Profiles';
+import NigeriaInfo from './components/NigeriaInfo';
+import FamousLandmarks from './components/FamousLandmarks';
+import FamousArtists from './components/FamousArtists';
+import NigerianMeals from './components/NigerianMeals';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Introduction />
+      <Profiles />
+      <NigeriaInfo />
+      <FamousLandmarks />
+      <FamousArtists />
+      <NigerianMeals />
+      <Footer />
     </div>
   );
 }
