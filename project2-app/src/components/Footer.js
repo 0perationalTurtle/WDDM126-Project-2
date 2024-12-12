662581a9524b100843809ea53b9a3444eebe6356
ANSELM OJIMADU <you@example.com>
2024-12-12 16:24:29 -0500

    Footer.js component Added

---
+++ project2-app/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Group 8 | All rights reserved</p>
+      <p>Follow us on social media:
+        <a href="#">Facebook</a> |
+        <a href="#">Twitter</a> |
+        <a href="#">Instagram</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
