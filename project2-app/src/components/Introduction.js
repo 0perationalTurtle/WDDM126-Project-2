dfb77b0fec961239bb50b8b5c2a3886e76582356
ANSELM OJIMADU <you@example.com>
2024-12-12 15:50:37 -0500

    Created an Introduction.js component

---
+++ project2-app/src/components/Introduction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Introduction() {
+  return (
+    <section id="introduction">
+      <h2>Meet Our Members</h2>
+      <p>Learn more about each member, their interests, and what makes them special.</p>
+    </section>
+  );
+}
+
+export default Introduction;
